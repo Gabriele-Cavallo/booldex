2c8c3b2a20d7430a5e214d1598c6ffda633b2f7c
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-12 20:37:53 +0200

    feat: add AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        name: 'AppHeader',
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<template>
+    <header class="h-[10vh] border-b-2 border-black">
+        <h1 class="h-full grid place-content-center">Booldex</h1>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
