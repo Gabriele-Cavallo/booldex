41670c37c97193d5971a2b458a1a6ab230ce96e4
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-13 01:05:27 +0200

    feat: add store.js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
-<script setup>
-  import AppHeader from './components/AppHeader.vue'
-  import AppPokedex from './components/AppPokedex.vue'
+<script>
+  import axios from 'axios';
+  import { store } from './store.js';
+  import AppHeader from './components/AppHeader.vue';
+  import AppPokedex from './components/AppPokedex.vue';
+
+  export default{
+    components: {
+      AppHeader,
+      AppPokedex
+    },
+    data(){
+      return{
+        store,
+      }
+    },
+    // methods: {
+    //   getAllPokemon(){
+    //     axios.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+    //     .then(response => this.store.pokedex = response.data.results)
+    //   }
+    // },
+    // mounted(){
+    //   this.getAllPokemon();
+    // },
+  }
 </script>
 
 <template>
