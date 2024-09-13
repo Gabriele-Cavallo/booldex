a7c06e7f27ea6f8aa3ab201f6e6a7a783a0b1690
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-13 21:21:04 +0200

    feat: add AppInfoPokemon component

---
+++ src/components/AppInfoPokemon.vue
@@ -0,0 +1,41 @@
+<script>
+import { store } from '../../src/store.js';
+
+    export default{
+        name: 'AppInfoPokemon',
+        data(){
+            return{
+                store
+            }
+        }
+    }
+</script>
+
+<template>
+    <section class="mb-2 h-4/6 border-2 p-4 border-black bg-green-600 rounded-md">
+        <div v-if="store.searchedPokemon">
+            <div>
+                <strong>Name</strong>: {{ store.searchedPokemon.name }}
+            </div>
+            <div>
+                <strong>Type</strong>: <span v-if="store.searchedPokemon.types">{{ store.searchedPokemon.types.map(type => type.type.name).join(', ') }}</span>
+            </div>
+            <div>
+                <strong>Height</strong>: "{{ store.searchedPokemon.height }}""
+            </div>
+            <div>
+                <strong>Weight</strong>: {{ store.searchedPokemon.weight }} lbs.
+            </div>
+            <div>
+                <div><strong>Stats</strong>:</div> 
+                <div class="flex items-center justify-between" v-for="stat in store.searchedPokemon.stats">
+                    <span>{{ stat.stat.name }}</span>
+                    <div class="h-full flex items-center w-1/2 border-2 border-black">
+                        <progress class="w-full h-[10px] bg-transparent" max="100" :value="stat.base_stat"></progress>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div v-else>No valid pokemon selected!</div>
+    </section>
+</template>
