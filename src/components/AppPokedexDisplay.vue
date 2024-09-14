66d1e7111d2c4936c348878eeaf3ef1e1d72cfa2
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-14 15:01:09 +0200

    fix: generic styles changes

--- src/components/AppPokedexDisplay.vue
+++ src/components/AppPokedexDisplay.vue
@@ -17,9 +17,9 @@ import { store } from '../../src/store.js';
 </script>
 
 <template>
-    <section class="h-1/3 grid place-items-center mb-4">
-        <div class="bg-slate-400 w-4/5 grid place-items-center h-full">
-            <div class="border-2 grid place-items-center border-black bg-white h-4/5 w-4/5 relative">
+    <section class="h-1/3 min-h-1/3 grid place-items-center mb-4">
+        <div class="bg-slate-400 border-2 border-black w-4/5 grid place-items-center h-full min-h-full">
+            <div class="border-2 grid place-items-center border-black bg-white h-4/5 w-4/5 relative overflow-y-hidden">
                 <img class="h-2/3" v-if="store.searchedPokemon" :src="store.searchedPokemon.sprites.other.showdown[gif]" alt="pokemon-gif">
                 <div v-else class="absolute inset-1 bg-[url('../../public/img/pokeball.png')] bg-center bg-contain bg-no-repeat"></div>
             </div>
--- src/components/AppPokemonFinder.vue
+++ src/components/AppPokemonFinder.vue
@@ -16,11 +16,30 @@ import { store } from '../store';
 
 <template>
     <section class="mb-4 h-[40px] flex">
-        <input placeholder="pokemon name" :value="userInput" @input="$emit('update:userInput', $event.target.value)"  class="w-1/3 h-full ps-1 outline-none border-2 border-r-0 rounded-s-sm border-red-950" type="text">
-        <button @click="$emit('searchPokemon')" class="bg-white border-2 h-full rounded-r-sm border-red-950 border-l-0 hover:rounded-sm hover:outline hover:outline-white hover:border-blue-700 hover:border-l-2 hover:outline-1 px-3">🔍</button>
+        <input 
+            placeholder="pokemon name" 
+            :value="userInput" 
+            @input="(event) => {$emit('update:userInput', event.target.value); $emit('resetResearch')}"  
+            class="w-1/3 h-full ps-1 outline-none border-2 border-r-0 rounded-s-sm border-red-950" 
+            type="text">
+        <button 
+            @click="$emit('searchPokemon')" 
+            class="bg-white border-2 h-full rounded-r-sm border-red-950 border-l-0 hover:rounded-sm hover:outline hover:outline-white hover:border-blue-700 hover:border-l-2 hover:outline-1 px-3">
+            🔍
+        </button>
         <div v-if="store.searchedPokemon" class="flex grow justify-end">
-            <button v-if="!store.myPokemon.includes(userInput[0].toUpperCase() + userInput.slice(1).toLowerCase()) && userInput.toLowerCase() === store.searchedPokemon.name" class="bg-slate-300 border-2 border-red-950 px-5" @click="$emit('catchPokemon')">Catch!</button>
-            <button v-else-if="store.myPokemon.includes(userInput[0].toUpperCase() + userInput.slice(1).toLowerCase()) && userInput.toLowerCase() === store.searchedPokemon.name" class="bg-slate-300 border-2 border-red-950 px-5" @click="$emit('releasePokemon')">Release!</button>
+            <button 
+                v-if="!store.myPokemon.includes(userInput[0].toUpperCase() + userInput.slice(1).toLowerCase()) &&
+                userInput.toLowerCase() === store.searchedPokemon.name" 
+                class="bg-slate-300 border-2 border-red-950 px-5" 
+                @click="$emit('catchPokemon')">Catch!
+            </button>
+            <button 
+                v-else-if="store.myPokemon.includes(userInput[0].toUpperCase() + userInput.slice(1).toLowerCase()) && 
+                userInput.toLowerCase() === store.searchedPokemon.name" 
+                class="bg-slate-300 border-2 border-red-950 px-5" 
+                @click="$emit('releasePokemon')">Release!
+            </button>
         </div>
     </section>
 </template>
